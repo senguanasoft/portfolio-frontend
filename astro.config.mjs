f4ed3ce962bf2f2242877ba309ca549d68d200c7
emiliosdev <sengua2000@outlook.com>
2025-11-01 22:42:31 -0500

    Primera version del portafolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://senguanasoft.github.io/portfolio-frontend/',
+  output: 'static',
   vite: {
     plugins: [tailwindcss()]
   }
